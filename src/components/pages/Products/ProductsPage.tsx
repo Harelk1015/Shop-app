9277d6851b151c4f38d3965e503817fd77ad022c
harelk1015 <harelk1015@gmail.com>
2022-03-24 02:51:45 +0200

    first commit

--- src/components/pages/Products/ProductsPage.tsx
+++ src/components/pages/Products/ProductsPage.tsx
@@ -30,6 +30,48 @@ const ProductsPage: React.FC = () => {
       imageUrl: 'https://cdn.shopify.com/s/files/1/2185/2813/products/W5561R_03482_b1_s1_a1_1_m18_750x.jpg?v=1622752753',
       category: { sex: 'man', kind: 'pants', sizes: [41, 42, 43] },
     },
+    {
+      _id: 4,
+      name: 'purple pants',
+      price: 29.99,
+      imageUrl: 'https://cdn.shopify.com/s/files/1/2185/2813/products/W5561R_03482_b1_s1_a1_1_m18_750x.jpg?v=1622752753',
+      category: { sex: 'man', kind: 'pants', sizes: [41, 42, 43] },
+    },
+    {
+      _id: 5,
+      name: 'purple pants',
+      price: 29.99,
+      imageUrl: 'https://cdn.shopify.com/s/files/1/2185/2813/products/W5561R_03482_b1_s1_a1_1_m18_750x.jpg?v=1622752753',
+      category: { sex: 'man', kind: 'pants', sizes: [41, 42, 43] },
+    },
+    {
+      _id: 6,
+      name: 'purple pants',
+      price: 29.99,
+      imageUrl: 'https://cdn.shopify.com/s/files/1/2185/2813/products/W5561R_03482_b1_s1_a1_1_m18_750x.jpg?v=1622752753',
+      category: { sex: 'man', kind: 'pants', sizes: [41, 42, 43] },
+    },
+    {
+      _id: 7,
+      name: 'purple pants',
+      price: 29.99,
+      imageUrl: 'https://cdn.shopify.com/s/files/1/2185/2813/products/W5561R_03482_b1_s1_a1_1_m18_750x.jpg?v=1622752753',
+      category: { sex: 'man', kind: 'pants', sizes: [41, 42, 43] },
+    },
+    {
+      _id: 7,
+      name: 'purple pants',
+      price: 29.99,
+      imageUrl: 'https://cdn.shopify.com/s/files/1/2185/2813/products/W5561R_03482_b1_s1_a1_1_m18_750x.jpg?v=1622752753',
+      category: { sex: 'man', kind: 'pants', sizes: [41, 42, 43] },
+    },
+    {
+      _id: 7,
+      name: 'purple pants',
+      price: 29.99,
+      imageUrl: 'https://cdn.shopify.com/s/files/1/2185/2813/products/W5561R_03482_b1_s1_a1_1_m18_750x.jpg?v=1622752753',
+      category: { sex: 'man', kind: 'pants', sizes: [41, 42, 43] },
+    },
   ];
 
   const asd = ['asd', 'asd'];
