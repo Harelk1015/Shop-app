7e7090293afee2d965636fccc9a34550728eb586
harelk1015 <harelk1015@gmail.com>
2022-05-04 09:23:50 +0300

    asd

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import { useEffect, useState } from 'react';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import { bindActionCreators } from 'redux';
 import { useDispatch, useSelector } from 'react-redux';
 import axios from 'axios';
@@ -8,37 +7,7 @@ import { ReducersState } from './store/reducers';
 import * as actionCreators from './store/reducers/actionCreator';
 
 import { User } from './utils/types';
-
-import Home from './components/pages/Home/Home';
-
-import Login from './components/pages/Login/Login';
-import Register from './components/pages/Register/Register';
-import Footer from './components/ui/Footer/Footer';
-import NavBar from './components/ui/NavBar/NavBar';
-import PageNotFound from './components/pages/404/PageNotFound';
-import Contact from './components/pages/Contact/Contact';
-import ProductsPage from './components/pages/ProductsPage/ProductsPage';
-import ProductPage from './components/pages/ProductPage/ProductPage';
-import Profile from './components/pages/Profile/Profile';
-import AdminPanel from './components/pages/Admin/AdminPanel';
-import Tickets from './components/pages/Tickets/Tickets';
-import Cart from './components/pages/Cart/Cart';
-import PageLoading from './components/ui/PageLoading/PageLoading';
-
-// const Home = React.lazy(() => import('./components/pages/Home/Home'));
-// const Login = React.lazy(() => import('./components/pages/Login/Login'));
-// const Register = React.lazy(() => import('./components/pages/Register/Register'));
-// const Footer = React.lazy(() => import('./components/ui/Footer/Footer'));
-// const NavBar = React.lazy(() => import('./components/ui/NavBar/NavBar'));
-// const PageNotFound = React.lazy(() => import('./components/pages/404/PageNotFound'));
-// const Contact = React.lazy(() => import('./components/pages/Contact/Contact'));
-// const ProductsPage = React.lazy(() => import('./components/pages/ProductsPage/ProductsPage'));
-// const ProductPage = React.lazy(() => import('./components/pages/ProductPage/ProductPage'));
-// const Profile = React.lazy(() => import('./components/pages/Profile/Profile'));
-// const AdminPanel = React.lazy(() => import('./components/pages/Admin/AdminPanel'));
-// const Tickets = React.lazy(() => import('./components/pages/Tickets/Tickets'));
-// const Cart = React.lazy(() => import('./components/pages/Cart/Cart'));
-// const PageLoading = React.lazy(() => import('./components/ui/PageLoading/PageLoading'));
+import AppView from './app.view';
 
 import './App.scss';
 
@@ -79,50 +48,7 @@ const App = () => {
 		},
 	);
 
-	if (isLoading) {
-		return <PageLoading />;
-	}
-
-	return (
-		<BrowserRouter>
-			<NavBar />
-			<Routes>
-				{/* Routes for all users */}
-				<Route path="/" element={<Home />} />
-				<Route path="/products" element={<ProductsPage />} />
-				<Route path="/product" element={<ProductPage />} />
-
-				{/* Routes for unauthenticated users */}
-				{!auth.isAuth && (
-					<>
-						<Route path="/login" element={<Login />} />
-						<Route path="/register" element={<Register />} />
-						<Route path="/*" element={<Login />} />
-					</>
-				)}
-
-				{/* Routes for Authenticated users */}
-				{auth.isAuth && (
-					<>
-						<Route path="/contact" element={<Contact />} />
-						<Route path="/profile" element={<Profile />} />
-						<Route path="/cart" element={<Cart />} />
-					</>
-				)}
-
-				{/* Routes for Admins */}
-				{auth.user?.role === 'admin' && (
-					<>
-						<Route path="/admin-panel" element={<AdminPanel />} />
-						<Route path="/ticket-page" element={<Tickets />} />
-					</>
-				)}
-
-				<Route path="/*" element={<PageNotFound />} />
-			</Routes>
-			<Footer />
-		</BrowserRouter>
-	);
+	return <AppView isLoading={isLoading} auth={auth} />;
 };
 
 App.displayName = 'App';
---
+++ src/app.view.tsx
@@ -0,0 +1,95 @@
+import React from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+
+import Home from './components/pages/Home/Home';
+import Login from './components/pages/Login/Login';
+import Register from './components/pages/Register/Register';
+import Footer from './components/ui/Footer/Footer';
+import NavBar from './components/ui/NavBar/NavBar';
+import PageNotFound from './components/pages/404/PageNotFound';
+import Contact from './components/pages/Contact/Contact';
+import ProductsPage from './components/pages/ProductsPage/ProductsPage';
+import ProductPage from './components/pages/ProductPage/ProductPage';
+import Profile from './components/pages/Profile/Profile';
+import AdminPanel from './components/pages/Admin/AdminPanel';
+import Tickets from './components/pages/Tickets/Tickets';
+import Cart from './components/pages/Cart/Cart';
+import PageLoading from './components/ui/PageLoading/PageLoading';
+
+import './App.scss';
+
+import { User } from './utils/types';
+
+// const Home = React.lazy(() => import('./components/pages/Home/Home'));
+// const Login = React.lazy(() => import('./components/pages/Login/Login'));
+// const Register = React.lazy(() => import('./components/pages/Register/Register'));
+// const Footer = React.lazy(() => import('./components/ui/Footer/Footer'));
+// const NavBar = React.lazy(() => import('./components/ui/NavBar/NavBar'));
+// const PageNotFound = React.lazy(() => import('./components/pages/404/PageNotFound'));
+// const Contact = React.lazy(() => import('./components/pages/Contact/Contact'));
+// const ProductsPage = React.lazy(() => import('./components/pages/ProductsPage/ProductsPage'));
+// const ProductPage = React.lazy(() => import('./components/pages/ProductPage/ProductPage'));
+// const Profile = React.lazy(() => import('./components/pages/Profile/Profile'));
+// const AdminPanel = React.lazy(() => import('./components/pages/Admin/AdminPanel'));
+// const Tickets = React.lazy(() => import('./components/pages/Tickets/Tickets'));
+// const Cart = React.lazy(() => import('./components/pages/Cart/Cart'));
+// const PageLoading = React.lazy(() => import('./components/ui/PageLoading/PageLoading'));
+
+interface IAppView {
+	isLoading: boolean;
+	auth: {
+		user: User;
+		isAuth: boolean;
+	};
+}
+
+const AppView: React.FC<IAppView> = ({ isLoading, auth }) => {
+	if (isLoading) {
+		return <PageLoading />;
+	}
+
+	return (
+		<BrowserRouter>
+			<NavBar />
+			<Routes>
+				{/* Routes for all users */}
+				<Route path="/" element={<Home />} />
+				<Route path="/products" element={<ProductsPage />} />
+				<Route path="/product" element={<ProductPage />} />
+
+				{/* Routes for unauthenticated users */}
+				{!auth.isAuth && (
+					<>
+						<Route path="/login" element={<Login />} />
+						<Route path="/register" element={<Register />} />
+						<Route path="/*" element={<Login />} />
+					</>
+				)}
+
+				{/* Routes for Authenticated users */}
+				{auth.isAuth && (
+					<>
+						<Route path="/contact" element={<Contact />} />
+						<Route path="/profile" element={<Profile />} />
+						<Route path="/cart" element={<Cart />} />
+					</>
+				)}
+
+				{/* Routes for Admins */}
+				{auth.user?.role === 'admin' && (
+					<>
+						<Route path="/admin-panel" element={<AdminPanel />} />
+						<Route path="/ticket-page" element={<Tickets />} />
+					</>
+				)}
+
+				<Route path="/*" element={<PageNotFound />} />
+			</Routes>
+			<Footer />
+		</BrowserRouter>
+	);
+};
+
+AppView.displayName = 'AppView';
+
+export default AppView;
